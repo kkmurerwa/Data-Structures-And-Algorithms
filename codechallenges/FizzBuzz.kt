d3263cec801bce2eca7e04b85644b5963add8f9c
Kenneth Murerwa <xwaxes@gmail.com>
2023-12-18 13:27:29 +0300

    feat: Add more code challenges

---
+++ codechallenges/FizzBuzz.kt
@@ -0,0 +1,17 @@
+package codechallenges
+
+fun main() {
+    val n = 15
+
+    for (i in 1..n) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            println("FizzBuzz")
+        } else if (i % 5 == 0) {
+            println("Buzz")
+        } else if (i % 3 == 0) {
+            println("Fizz")
+        } else {
+            println(i)
+        }
+    }
+}
