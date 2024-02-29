200b071ff99c632611e42017a7739b7935dd73c6
Kenneth Murerwa <xwaxes@gmail.com>
2024-02-29 10:50:16 +0300

    add more challenges

---
+++ codechallenges/leetcode/LongestConsecutiveSequence.kt
@@ -0,0 +1,52 @@
+package codechallenges.leetcode
+
+/**
+ * LeetCode 128: Longest Consecutive Sequence
+ *
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ *
+ * You must write an algorithm that runs in O(n) time.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [100,4,200,1,3,2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * Example 2:
+ *
+ * Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ * Output: 9
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ */
+fun main() {
+    val input1 = intArrayOf(100, 4, 200, 1, 3, 2)
+    println("${longestConsecutive(input1)} expected: 4")
+}
+
+fun longestConsecutive(nums: IntArray): Int {
+    val set = nums.toSet()
+
+    var longestStreak = 0
+    for (i in nums) {
+        if (!set.contains(i - 1)) {
+            var currentNum = i
+            var currentStreak = 1
+
+            while (set.contains(currentNum + 1)) {
+                currentNum++
+                currentStreak++
+            }
+
+            longestStreak = longestStreak.coerceAtLeast(currentStreak)
+        }
+    }
+
+    return longestStreak
+}
